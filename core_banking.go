81993f2befb62ce5f976d2031d67364103afd243
Lawrence Jones <lawrjone@gmail.com>
2019-07-14 22:33:26 +0100

    initial commit

---
+++ core_banking.go
@@ -0,0 +1,29 @@
+package main
+
+import "time"
+
+var (
+	MarkPaymentsAsPaidMeetsDeadline = BatchProcessingSLO{
+		BaseSLO:  BaseSLO{"MarkPaymentsAsPaid meets deadline", 0.1},
+		Labels:   []string{"namespace", "release"},
+		Deadline: time.Duration(2) * time.Hour,
+		Volume: `
+1.5 * max_over_time(
+  (
+    sum by (namespace, release) (
+      increase(paysvc_mark_payments_as_paid_marked_as_paid_total[8h])
+    )
+  )[60d:1h]
+)
+		`,
+		Throughput: `
+sum by (namespace, release) (
+  rate(paysvc_mark_payments_as_paid_marked_as_paid_total[1m])
+) > 0
+		`,
+	}
+)
+
+func init() {
+	MustRegister(MarkPaymentsAsPaidMeetsDeadline.Rules()...)
+}
