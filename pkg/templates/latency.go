e944fe46e13bde3ff52da97e1d82649bcfaeb83d
Joao Amaro <joaoamaro@gocardless.com>
2019-08-07 16:32:36 +0100

    Updates latency tmplt with mapping between req classes & latency buckets

--- pkg/templates/latency.go
+++ pkg/templates/latency.go
@@ -20,6 +20,33 @@ var (
 	)
 )
 
+// This map contains a mapping between request classes and the latency buckets
+// we use in Prometheus. 
+// 
+// It assumes that the observation metric is an histogram where the following
+// buckets exist: 0.1, 0.25, 0.5, 1, 2.5, 5 and 10.
+//
+// The bucket values were chosen based on the performance of payments-service
+// over a  month. The spreadsheet linked below includes the latency percentiles
+// of payments-service for each one of the routes over a month. We chose the
+// bucket values below keeping in mind that:
+// - We wanted to choose a set of buckets we could use across our 95th and 99th
+//   latency percentiles
+// - The buckets are generic and have an even distribution across all of routes,
+//   i.e. all buckets apply to an similar number of routes so that there are no
+//   special cases
+//
+// https://docs.google.com/spreadsheets/d/1CAe6gpgZdjYjBy44bdYSBD0ZA8yxSkMkiqjLiZ_8NRo/edit#gid=15427288
+var requestClassToLatencyBucket = map[string]string{
+  "fast++": "0.1", // 100 ms
+  "fast+": "0.25", // 250 ms
+  "fast": "0.5",   // 500 ms
+  "ok": "1",       // 1s
+  "slow": "2.5",   // 2.5s
+  "slow+": "5",    // 5s
+  "slow++": "10",  // 10s
+}
+
 func init() {
 	MustRegisterTemplate(LatencySLO{}, LatencyTemplateRules...)
 }
@@ -59,7 +86,8 @@ func (l LatencySLO) Rules() []rulefmt.Rule {
 		forIntervals(AlertWindows, rulefmt.Rule{
 			Record: "job:slo_latency_observation:rate%s",
 			Labels: l.joinLabels(map[string]string{"request_class": l.RequestClass}),
-			Expr:   fmt.Sprintf(l.Observation, l.RequestClass, "%s"),
+			// TODO what if the class is invalid? Do we want to return an error?
+			Expr:   fmt.Sprintf(l.Observation, requestClassToLatencyBucket[l.RequestClass], "%s"),
 		}),
 	)
 }
